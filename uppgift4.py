ff40f604ca31fbf33afa8029ea9131a8f2b8f268
Camelia Ciuca <camelia1414@gmail.com>
2025-01-08 18:32:10 +0100

    Math

---
+++ uppgift4.py
@@ -0,0 +1,39 @@
+'''
+1a Det är ca 470 km mellan Stockholm och Göteborg. Skriv ett program som räknar ut hur lång tid det tar att köra från Stockholm till Göteborg. Du behöver fråga användaren hur fort man ska köra, i km/h.
+Tips: omvandla till m/s genom att dela med 3,6. Sedan kan du använda formeln: tid = sträcka / hastighet.
+
+1b Gör så att programmet svarar i minuter i stället för sekunder.
+
+1c (svårare) Nu ska programmet svara i hela timmar och minuter.
+Tips: använd operatorerna // och %. Heltalsdivision // dividerar och avrundar nedåt till närmaste heltal. Procent % räknar ut resten vid division med heltal. Exempel:
+3 // 2 == 1      (3 / 2 == 1.5, avrundar nedåt)
+60 % 60 == 0  (ingen rest)
+70 % 60 == 10  (10 i rest)
+Be en AI förklara heltalsdivision och modulo i Python om du känner dig osäker!
+
+
+2 Skriv ett program som räknar ut längden på hypotenusan i en rätvinklig triangel. Användaren behöver skriva in längden på de två kortare sidorna.
+Tips 1: fråga en AI om formeln Pythagoras sats. Men var noga med att inte fråga efter kod som löser uppgiften!
+Tips 2: räkna ut roten med math.sqrt().
+
+Som testdata kan du använda triangeln med sidorna 3, 4 och 5:
+
+
+
+3a Skriv ett program som talar om dagens datum.
+Resurs: Hantera datum i Python
+
+3b (svårare) Ändra programmet så att det kan tala om vilket datum det är om 7 dagar.
+
+Vad är code review?
+Alla i gruppen visar i tur ordning upp hur långt man har kommit med uppgiften. När man inte visar, har man som uppgift att ge konstruktiv feedback. Observera att man inte behöver vara färdig! Code review kan vara ett mycket bra stöd för att komma vidare.
+
+Den som visar upp sin kod:
+Kör programmet (oavsett om det blir fel eller inte)
+Visar upp kodfilerna
+
+De som ger feedback:
+Frågar om det är något man inte förstår
+Ger förslag på hur koden kan förbättras
+
+'''
