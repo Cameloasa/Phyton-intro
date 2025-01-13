50b2b5d7a6b349c688b942bd3dac7a478c6b596e
Camelia Ciuca <camelia1414@gmail.com>
2025-01-13 13:52:12 +0100

    Changes in naming some variables from Swedish till English

--- uppgift2.py
+++ uppgift2.py
@@ -3,7 +3,7 @@
 ticket_price = 100  # biljettpris
 money_on_hand= 200  # pengar på fickan
 print("Det blir " + str(ticket_price - ticket_price) + " kronor över.") # omvandla variables to concatenate str()
-half_of_money_left = (200 - 100) / 2 # () aritmetik
+half_of_money_left = (money_on_hand - ticket_price) / 2 # () aritmetik
 print("Hälften är: " + str(half_of_money_left))
 
 #Kom på bättre namn, i stället för x, y och z.
