73ee15aa505abd2f6fe9e7eaf328cca6a19af479
Camelia Ciuca <camelia1414@gmail.com>
2025-01-13 13:54:27 +0100

    Changes in naming some variables from Swedish till English

--- uppgift2.py
+++ uppgift2.py
@@ -2,7 +2,7 @@
 #Rätta eventuella fel. Vad gör koden?
 ticket_price = 100  # biljettpris
 money_on_hand= 200  # pengar på fickan
-print("Det blir " + str(ticket_price - ticket_price) + " kronor över.") # omvandla variables to concatenate str()
+print("Det blir " + str(money_on_hand - ticket_price) + " kronor över.") # omvandla variables to concatenate str()
 half_of_money_left = (money_on_hand - ticket_price) / 2 # () aritmetik
 print("Hälften är: " + str(half_of_money_left))
 
